e55313a01d95dd2ac9aac52d47e5ec366d062599
ogalush <ogalush@MacBook1.local>
2015-06-13 21:14:37 +0900

    インストール手順に沿って入れたconfigを反映

---
+++ controller/admin-openrc.sh
@@ -0,0 +1,9 @@
+export OS_PROJECT_DOMAIN_ID=default
+export OS_USER_DOMAIN_ID=default
+export OS_PROJECT_NAME=admin
+export OS_TENANT_NAME=admin
+export OS_USERNAME=admin
+export OS_PASSWORD=password
+export OS_AUTH_URL=http://192.168.0.200:35357/v3
+export OS_IMAGE_API_VERSION=2
+export OS_VOLUME_API_VERSION=2
---
+++ controller/demo-openrc.sh
@@ -0,0 +1,9 @@
+export OS_PROJECT_DOMAIN_ID=default
+export OS_USER_DOMAIN_ID=default
+export OS_PROJECT_NAME=demo
+export OS_TENANT_NAME=demo
+export OS_USERNAME=demo
+export OS_PASSWORD=password
+export OS_AUTH_URL=http://192.168.0.200:5000/v3
+export OS_IMAGE_API_VERSION=2
+export OS_VOLUME_API_VERSION=2
